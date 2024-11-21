ba4bd384c80840e647d6f4c324286aa4ea2b27b1
Mikael Brockman <mikael@brockman.se>
2024-11-21 05:38:29 +0200

    test: Refactor request test to use nested test step

--- test/request_test.ts
+++ test/request_test.ts
@@ -5,13 +5,17 @@ Deno.test("HTTP Request to RDF", async (t) => {
   const request = new Request("http://example.com/api/users/123");
   const store = requestToStore(request);
 
-  await assertQuery(t, store, `
+  await t.step("converts URL path to RDF request", async (step) => {
+    await assertQuery(
+      store,
+      `
     @prefix http: <http://www.w3.org/2011/http#> .
     
     {
       ?request a http:Request;
         http:requestURI "/api/users/123" .
-    }`, 
-    "converts URL path to RDF request"
-  );
+    }`,
+      step.name,
+    );
+  });
 });
