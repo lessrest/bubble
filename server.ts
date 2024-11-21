5936982735142244e0071a75006d67569bfae671
Mikael Brockman <mikael@brockman.se>
2024-11-21 18:54:54 +0100

    refactor: Improve signal handling in server.ts; log messages on SIGINT and SIGTERM

--- server.ts
+++ server.ts
@@ -3,18 +3,27 @@ import { handleWithRules, withGroundFacts } from "./src/utils.ts";
 const store = await withGroundFacts(
   await Deno.readTextFile("./ground-facts.ttl")
 );
+
 const inboxRules = await Deno.readTextFile("./rules/inbox.n3");
 const htmlRules = await Deno.readTextFile("./rules/html.n3");
+
 const rules = inboxRules + "\n" + htmlRules;
 
-export async function handler(req: Request): Promise<Response> {
+export function handler(req: Request): Promise<Response> {
   return handleWithRules(req, rules, store, store);
 }
 
 if (import.meta.main) {
   const controller = new AbortController();
   const server = Deno.serve({ port: 8000, signal: controller.signal }, handler);
-  Deno.addSignalListener("SIGINT", () => controller.abort());
+  Deno.addSignalListener("SIGINT", () => {
+    console.log("SIGINT received");
+    controller.abort();
+  });
+  Deno.addSignalListener("SIGTERM", () => {
+    console.log("SIGTERM received");
+    controller.abort();
+  });
   server.finished.then(() => {
     console.log("Server stopping...");
   });
